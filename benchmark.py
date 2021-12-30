83ec2c9c2766a22fa5c07033ddfac50595745434
nicehero <85948597@qq.com>
2021-12-30 12:51:39 +0800

    nicehero version

--- benchmark.py
+++ benchmark.py
@@ -7,7 +7,21 @@ opts = ["insert","find","erase&insert","erase"]
 fp = open("result.txt","r")
 ll = fp.readlines()
 fp.close()
-plt.subplots(3,3, figsize=(14, 9))
+fig,_ = plt.subplots(3,3, figsize=(14, 9))
+axs = []
+def on_move(event):
+	# get the x and y pixel coords
+	x, y = event.x, event.y
+	if event.inaxes:
+		ax = event.inaxes  # the axes instance
+		for a in axs:
+			if a[0] == ax:
+				a[1].set_position((event.xdata, event.ydata))
+				a[1].set_text(str(int(event.ydata)))
+			else:
+				a[1].set_text("")
+		#print('data coords %f %f %f %f' % (x, y,event.xdata, event.ydata))
+		fig.canvas.draw_idle()
 i = 1
 for opt in opts:
 	y = []
@@ -37,7 +51,7 @@ for opt in opts:
 		y1 = y1[2:]
 		x1 = x1[:-2]
 		x2 = x2[2:]
-	plt.subplot(2,2,i)
+	ax = plt.subplot(2,2,i)
 	i += 1
 	plt.xticks(x1,x2)
 	y2 = [np.max(y),np.mean(y),np.median(y),np.min(y)]
@@ -47,5 +61,9 @@ for opt in opts:
 	plt.title(opt)
 	plt.xlabel("Size")
 	plt.ylabel("QPS(w)")
+	t = plt.text(0,0,"",fontsize = 10)
+	axs.append([ax,t])
 	plt.legend()
+plt.connect('motion_notify_event', on_move)
+#plt.savefig("plt.svg")
 plt.show()
